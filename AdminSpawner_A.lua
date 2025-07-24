84f198732b1e406b547df5163362621fb699655c
ter4x <anonymous7002002@gmail.com>
2025-07-24 18:23:59 +0800

    Update AdminSpawner_A.lua

--- AdminSpawner_A.lua
+++ AdminSpawner_A.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/tgpTSCVsF5cJR7E7/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/LrEl6ag60L0mkb5m/raw", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
